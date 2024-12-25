a4a01b50a8d01529ebc96b60572714d6390b79ce
713068_Ashim <ashimaakash1997@gmail.com>
2024-12-25 21:59:56 +0530

    rendered products list under Collection page

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,17 +1,27 @@
 //import React from 'react'
-
+import { useContext, useEffect } from "react";
 import { useState } from "react";
 import { assets } from "../assets/frontend_assets/assets";
-// import ShopContext from "../context/ShopContext";
+import ProductItem from "../components/ProductItem";
+import Title from "../components/Title";
+import { ShopContext } from "../context/ShopContext";
 
 const Collection = () => {
-  // const { products } = useContext(ShopContext);
+  const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
+
+  useEffect(() => {
+    setFilterProducts(products);
+  });
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
       {/**Filter Options */}
       <div className="min-w-60">
-        <p onClick={()=> setShowFilter(!showFilter)} className="my-2 text-xl flex items-center cursor-pointer gap-3">
+        <p
+          onClick={() => setShowFilter(!showFilter)}
+          className="my-2 text-xl flex items-center cursor-pointer gap-3"
+        >
           FILTERS
           <img
             className={`h-3 sm:hidden ${showFilter ? "rotate-90" : ""}`}
@@ -59,6 +69,30 @@ const Collection = () => {
               Winterwear
             </p>
           </div>
+        </div>
+      </div>
+      {/**Right Side of Collection Component */}
+      <div className="flex-1">
+        <div className="flex justify-between text-base sm:text-2xl mb-4">
+          <Title text1={"ALL"} text2={"COLLECTIONS"} />
+          {/**Product Sort (by Price) */}
+          <select className="border-2 border-gray-400 text-sm px-2">
+            <option value="relevant">Sort By: Relevant</option>
+            <option value="low-high">Sort By: Low to High</option>
+            <option value="high-low">Sort By: High to Low</option>
+          </select>
+        </div>
+        {/** Rendering the list of products */}
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+          {filterProducts.map((item, index) => (
+            <ProductItem
+              key={index}
+              id={item._id}
+              name={item.name}
+              image={item.image}
+              price={item.price}
+            />
+          ))}
         </div>
       </div>
     </div>

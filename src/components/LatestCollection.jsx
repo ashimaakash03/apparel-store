4a1e57ad3b513aa21be5b2fb6ee4c271a1899b5d
713068_Ashim <ashimaakash1997@gmail.com>
2024-12-25 16:00:07 +0530

    added logic for the sidenav to highlight the active link

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -16,7 +16,9 @@ const LatestCollection = () => {
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
         <Title text1={"LATEST"} text2={"COLLECTIONS"} />
-        
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Latest Collection of Apparels for EVERYBODY !!!
+        </p>
       </div>
       {/** Rendering Products */}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">

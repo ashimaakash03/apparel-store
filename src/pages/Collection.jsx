07c6f827b3249cf43cabb18e7a7af6f0535e6c6f
713068_Ashim <ashimaakash1997@gmail.com>
2024-12-25 19:00:07 +0530

    added functionality to show / hide filters in small screen

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,9 +1,68 @@
 //import React from 'react'
 
-const Collection = () => {
-  return (
-    <div>Collection</div>
-  )
-}
+import { useState } from "react";
+import { assets } from "../assets/frontend_assets/assets";
+// import ShopContext from "../context/ShopContext";
 
-export default Collection
+const Collection = () => {
+  // const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+      {/**Filter Options */}
+      <div className="min-w-60">
+        <p onClick={()=> setShowFilter(!showFilter)} className="my-2 text-xl flex items-center cursor-pointer gap-3">
+          FILTERS
+          <img
+            className={`h-3 sm:hidden ${showFilter ? "rotate-90" : ""}`}
+            src={assets.dropdown_icon}
+            alt=""
+          />
+        </p>
+        {/** Category Filters */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+            showFilter ? "" : "hidden"
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Men"} /> Men
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Women"} /> Women
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Kids"} /> Kids
+            </p>
+          </div>
+        </div>
+        {/** Sub Category Filters */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 my-5 ${
+            showFilter ? "" : "hidden"
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium">SUB CATEGORIES</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Topwear"} />{" "}
+              Topwear
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Bottomwear"} />{" "}
+              Bottomwear
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Winterwear"} />{" "}
+              Winterwear
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Collection;

29f725bf84a6ff9eb5129904bd38db58f28f2041
713068_Ashim <ashimaakash1997@gmail.com>
2024-12-25 16:22:18 +0530

    rendered products under BestSeller component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 //import React from 'react'
 
+import BestSeller from "../components/BestSeller";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
 
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
     </div>
   );
 };

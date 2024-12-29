180c220a563f91e2a4d23f2a54f852b19ce2044b
713068_Ashim <ashimaakash1997@gmail.com>
2024-12-29 14:09:52 +0530

    completed building UI for the Login page

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,17 @@ import Contact from "./pages/Contact";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      <ToastContainer/>
+      <ToastContainer />
       <Navbar />
       <SearchBar />
       <Routes>
@@ -30,7 +29,6 @@ const App = () => {
         <Route path="/product/:productId" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/orders" element={<Orders />} />
       </Routes>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,60 @@
 //import React from 'react'
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { useState } from "react";
 
-export default Login
+const Login = () => {
+  const [currentState, setCurrentState] = useState("Sign Up");
+  return (
+    <form className="flex flex-col items-center w-[90%] sm:w-[96%] m-auto mt-14 gap-4 text-gray-700">
+      <div className="inline-flex items-center gap-3 mt-10 mb-2">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[2px] w-8 bg-gray-700" />
+      </div>
+      {currentState === "Sign In" ? (
+        ""
+      ) : (
+        <input
+          type="text"
+          className="w-[50%] px-3 py-2 border border-gray-700 rounded-lg"
+          placeholder="John Doe"
+          required
+        />
+      )}
+      <input
+        type="email"
+        className="w-[50%] px-3 py-2 border border-gray-700 rounded-lg"
+        placeholder="abc@xyz.com"
+        required
+      />
+      <input
+        type="password"
+        className="w-[50%] px-3 py-2 border border-gray-700 rounded-lg"
+        placeholder="password"
+        required
+      />
+      <div className="w-[50%] flex justify-between text-sm mt-[10px]">
+        <p className="cursor-pointer">Forgot Password?</p>
+        {currentState === "Sign Up" ? (
+          <p
+            className="cursor-pointer"
+            onClick={() => setCurrentState("Sign In")}
+          >
+            Sign In Here...
+          </p>
+        ) : (
+          <p
+            className="cursor-pointer"
+            onClick={() => setCurrentState("Sign Up")}
+          >
+            Sign Up Here...
+          </p>
+        )}
+      </div>
+      <button className="cursor-pointer bg-black text-white px-8 py-4 mt-3 rounded-full">
+        {currentState === "Sign In" ? "Login" : "Register"}
+      </button>
+    </form>
+  );
+};
+
+export default Login;
